de076b46a906b9faf5008fc700d67657716727ad
Naman <namannigam12@gmail.com>
2021-06-07 23:40:45 +0530

    thought process

--- src/main/java/edu/bit/designing/maze/MazeGenerator.java
+++ src/main/java/edu/bit/designing/maze/MazeGenerator.java
@@ -4,6 +4,29 @@ import java.util.Random;
 
 public class MazeGenerator implements Maze {
 
+    /**
+     * [ 1, 1, 1, 1, 1]
+     * [ 1, 1, 1, 1, 1]
+     * [ 1, 1, 1, 1, 1]
+     * <p>
+     * [ 1, 1, 1, 1, 1]
+     * [ 1, 1, 0, 1, 1]
+     * [ 1, 1, 1, 1, 1]
+     * <p>
+     * // represent each value as a node of a graph
+     * // source to destination there is always a path possible [0,0] [M,N]
+     * <p>
+     * 1-> [0,1] 0 / 1  ->
+     * -> [1,0] [1,0/1] ->
+     * <p>
+     * root -> right -> right  (until N)
+     * -> down
+     * -> down
+     * <p>
+     * -> until(M)
+     * <p>
+     * 1 -> 1 -> 0/1
+     */
     public int[][] getMaze(int rows, int cols) {
         int[][] maze = new int[rows][cols];
         while (!hasPath(maze)) { // combinations of R*C, goes exponential
