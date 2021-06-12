7a647d93ebd49bed9063f87611165d7ee8c43745
Naman <namannigam12@gmail.com>
2021-06-12 10:29:13 +0530

    book my show design for bookig

--- src/main/java/edu/bit/designing/bookmyshow/package-info.java
+++ src/main/java/edu/bit/designing/bookmyshow/package-info.java
@@ -1,1 +1,61 @@
 package edu.bit.designing.bookmyshow;
+
+/**
+ * book my show
+ * => search movie (text: string) : Movie =>
+ * => booking seats for a movie ()
+ * <p>
+ * search of a movie => theatre => city =>
+ * <p>
+ * Movie:
+ * String movieId;
+ * String name;
+ * <p>
+ * City:
+ * String cityId; // used fo geolocation
+ * List<Threatre> theatres;
+ * <p>
+ * Theatre:
+ * String theatreId;
+ * List<Screen> screens;
+ * <p>
+ * Screen:
+ * String screenId;
+ * String theatreId;
+ * List<Show> slot;
+ * <p>
+ * Show:
+ * String showId;
+ * String movieId;
+ * Duration duration;
+ * List<Seat> seats;
+ * <p>
+ * Seat
+ * String seatId;
+ * Map<String, Boolean> availablity;
+ * <p>
+ * ShowSeat
+ * String seatId;
+ * String showId;
+ * Boolean availablity;
+ * <p>
+ * User
+ * String accountId;
+ * <p>
+ * Booking:
+ * String accountId;
+ * String showId;  // movie, duration
+ * List<Seat> seats; // these are seats booked for
+ * <p>
+ * <p>
+ * // 1Mil users are booking a seat and all of them are looking movieId
+ * <p>
+ * <p>
+ * 1 				2 					3 					4 				5
+ * availability	availability	availability		availability	availability
+ * <p>
+ * <p>
+ * acquireLock
+ * bookSeat
+ * releaseLock
+ */
